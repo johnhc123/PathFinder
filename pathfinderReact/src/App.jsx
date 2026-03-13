871a5d5695189d0f8bf61e4f1e90b5137a05c5e5
JulianMG525 <superjuliang@egmail.com>
2026-03-13 09:51:35 -0700

    shoe

--- pathfinderReact/src/App.jsx
+++ pathfinderReact/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
+import ShoeImage from "./assets/Shoe.jpg"
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <Link to="/about">About</Link> |{" "}
         <Link to="/profile">Profile</Link>
       </nav>
-
+<img src={ShoeImage}/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
