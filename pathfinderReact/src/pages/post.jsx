d79bb5835d5131fd9ce9d1b52e1be5918626845f
JulianMG525 <superjuliang@egmail.com>
2026-03-20 10:02:14 -0700

    postpagestart

---
+++ pathfinderReact/src/pages/post.jsx
@@ -0,0 +1,15 @@
+function post () {
+
+<form action={post}>
+   <h1>Post A Review</h1>
+   <p>Upload your images!</p>
+   <input type="text" id="trail" placeholder="Type Here"></input>
+   
+   
+    <input name="query" />
+    <button type="submit">Post</button>
+</form>
+
+
+
+}
