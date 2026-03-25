3dd615565cde0a84dfaab2266decacc1dd4e6f58
Jonathan Chen <jonachen12@gmail.com>
2026-03-25 09:44:41 -0700

    replacement w/ actual trail

--- pathfinderReact/src/BayT.jsx
+++ pathfinderReact/src/BayT.jsx
@@ -1,9 +1,9 @@
-import car from "./assets/car.gif";
+import cooltrail from "./assets/cooltrail.jpg";
 import "react-multi-carousel/lib/styles.css";
 import Carousell from "./assets/Carousell";
 
 function BayT() {
-  return <div>  <h1>Bay Trails Page</h1>   <p>Different trails to explore in the Bay Area!</p>    <Carousell img1={car} img2={car} img3={car} img4={car} img5={car}/>
+  return <div>  <h1>Bay Trails Page</h1>   <p>Different trails to explore in the Bay Area!</p>    <Carousell img1={cooltrail} img2={cooltrail} img3={cooltrail} img4={cooltrail} img5={cooltrail}/>
 </div>;
 }
 
