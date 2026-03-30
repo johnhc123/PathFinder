e9b6cd8ec79b880e83f92e17c070825cd469891e
Jonathan Chen <jonachen12@gmail.com>
2026-03-30 09:52:02 -0700

    form tweak

--- pathfinderReact/src/TripPlanner.jsx
+++ pathfinderReact/src/TripPlanner.jsx
@@ -1,11 +1,14 @@
 import Card from "./assets/Card.jsx";
+import TrailForm from "./assets/TrailSelectform.jsx"
 
 function TripPlanner() {
   return <div>
     <h1 className="text-4xl font-bold text-center my-8">Trip Planner</h1> 
     <Card
     title="Trip Planner!"
-    description="Here you can create custom trip itineraries!"/></div>;
-}
+    description="Here you can create custom trip itineraries!"/>;
 
+<TrailForm></TrailForm>
+</div>
+}
 export default TripPlanner;
