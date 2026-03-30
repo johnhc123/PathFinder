523e2bee42ecd4ce3aa3b3dfd1dc5c069e90aa6d
Jonathan Chen <jonachen12@gmail.com>
2026-03-30 16:37:44 -0700

    centering tweak

--- pathfinderReact/src/TripPlanner.jsx
+++ pathfinderReact/src/TripPlanner.jsx
@@ -4,11 +4,15 @@ import TrailForm from "./assets/TrailSelectform.jsx"
 function TripPlanner() {
   return <div>
     <h1 className="text-4xl font-bold text-center my-8">Trip Planner</h1> 
+    <div className="flex items-center justify-center">
     <Card
     title="Trip Planner!"
-    description="Here you can create custom trip itineraries!"/>;
+    description="Here you can create custom trip itineraries!"/>; <br></br>
+    
+    <TrailForm></TrailForm>
+    
 
-<TrailForm></TrailForm>
+</div>
 </div>
 }
 export default TripPlanner;
